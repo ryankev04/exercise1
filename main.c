50460ef8737abce8d9f1ba905d92c3e07f8c2611
ryankev04 <ngaboryan5@gmail.com>
2026-09-07 18:41:53 +0200

    USART using ATMega162 microcontroller.

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Exercise_1.c
+ *
+ * Created: 07.09.2026 18:12:05
+ * Author : ngabo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
